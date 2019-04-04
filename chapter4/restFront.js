98a80569980bc799582f592a65202003e61e2b0e
YongSeok <qazwsx1455@gmail.com>
2019-04-04 16:51:42 +0900

    5st Week - 1

--- chapter4/restFront.js
+++ chapter4/restFront.js
@@ -78,3 +78,147 @@ document.getElementById('form').addEventListener('submit', function (e) {
   xhr.send(JSON.stringify({ name: name }));
   e.target.username.value = '';
 });
+
+
+// //Promise
+// function getUser() { // 로딩시 사용자 가져오는 함수
+//   //fetch: ajax기능을 위한 최시 라이브러리
+//   //fetch('url주소', 옵션객체)
+//   fetch('/users', {
+//       method: 'GET'
+//     }) //hhtp://localhost:8085/users 호출
+//     .then((response) => { //서버로부터의 users 객체를 포함한 프로미스
+//       if (response.status == '200') {
+//         return response.json();
+//       }
+//     })
+//     .then((resJson) => {
+//       showUsers(resJson, document.querySelector('list'));
+//     })
+//     .catch((error) => {
+//       console.error('fetch 호출에서 에러발생: ' + error.message);
+
+//     });
+// }
+
+// function showUsers(users, location) {
+//   //users: resJson
+//   //location: document.querySelector('#list')
+//   location.innerHTML = '';
+//   Object.keys(users).map((key) => {
+//     var userDiv = document.createElement('div');
+//     //<div></div>
+//     var span = document.createElement('span');
+//     //<span></span>
+//     span.textContent = users[key]; //연관배열
+//     //<span>users[key]의 값</span>
+
+//     var edit = createModifyBtn(key);
+//     //<div><span>users[key]의 값</span></div><button>수정</button></div>
+
+//     var remove = createModifyBtn(key);
+//     //<div><span>users[key]의 값</span></div><button>수정</button></div>
+
+//     userDiv.appendChild(span);
+//     //<div><span>users[key]의 값</span></div>
+
+//     userDiv.appendChild(edit);
+//     //<div><span>users[key]의 값</span></div><button>수정</button></div>
+
+//     userDiv.appendChild(remove);
+//     //<div><span>users[key]의 값</span></div><button>수정</button></div>
+//     //<button>삭제</button>
+//     location.appendChild(userDiv);
+//   });
+// }
+
+// function createModifyBtn(key) {
+//   var edit = document.createElement('button');
+//   //  <button></button>
+//   edit.textContent = '수정';
+  
+//   //<buton>수정</button>
+//   edit.addEventListener('click', () => {
+//     var name = prompt('바꿀 이름을 입력하세요');
+//     if (!name) {
+//       return alert('이름을 반드시 입력하셔야 합니다.');
+//     }
+//     fetch('/users/' + key, {
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       method: "PUT",
+//       body: JSON.stringify({
+//         name //name:name과 같음
+//       })
+//     }).then((response) => {
+//       if (response.status == '200') {
+//         return response.json();
+//       }
+//     }).then((resJson) => {
+//       console.log(resJson);
+//       showUsers(resJson, document.querySelector('#list'));
+//     }).catch((error) => {
+//       console.error('fetch호출에서 에러발생: ' + error.message);
+//     });
+//   });
+//   return edit;
+// }
+
+// function createDelBtn(key) {
+//   var remove = document.createElement('button');
+//   remove.textContent = '삭제';
+//   remove.addEventListener('click', () => {
+//     fetch('/users/' + key, {
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       method: "DELETE",
+//     }).then((response) => {
+//       if (response.status == '200') {
+//         return response.json();
+//       }
+//     }).then((resJson) => {
+//       console.log(resJson);
+//       showUsers(resJson, document.querySelector('#list'));
+//     }).catch((error) => {
+//       console.error('fetch호출에서 에러발생: ' + error.message);
+//     });
+//   });
+//   return remove;
+// }
+
+// window.onload = getUser; // 로딩 시 getUsers 호출
+// //window.onload = 자바의 메인역할
+// //폼 제출
+// document.querySelector('#form').addEventListener('submit', (e) => {
+//   //e는 form태그에 발생한 이벤트 객체
+//   e.preventDefault(); //막는다 default를 막는다!!
+//   //default면 클릭에 대한 이벤트를 막는다.
+//   var name = e.target.username.value;
+//   //e.target.username는 e.target 객체(form객체)의 input태그 객체
+//   if (!name) {
+//     return alert('이름을 입력하세요');
+//   }
+//   fetch('/users', {
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     method: "POST",
+//     body: JSON.stringify({
+//       name: name
+//     })
+//   }).then((response) => {
+//     if (response.status == '201') {
+//       return response.json();
+//     }
+//   }).then((result) => {
+//     showUsers(result, document.querySelector('#list'));
+//   }).catch((error) => {
+//     console.error('fetch 호출에서 에러발생: ' + error.message);
+//   });
+//   e.target.username.value = ''; //더블클릭 막기 이중생성 막기
+// });
--- chapter4/restServer.js
+++ chapter4/restServer.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const users = {};
+const users = {}; // 등록되는 사용자 정보를 저장하는 객체
 
 http.createServer((req, res) => {
   if (req.method === 'GET') {
@@ -19,7 +19,8 @@ http.createServer((req, res) => {
         }
         res.end(data);
       });
-    } else if (req.url === '/users') {
+    } else if (req.url === '/users') { 
+      //  fetch('/users', { method: 'GET' })  이게 일로옴
       return res.end(JSON.stringify(users));
     }
     return fs.readFile(`.${req.url}`, (err, data) => {
@@ -32,8 +33,8 @@ http.createServer((req, res) => {
   } else if (req.method === 'POST') {
     if (req.url === '/users') {
       let body = '';
-      req.on('data', (data) => {
-        body += data;
+      req.on('data', (fromClientData) => {
+        body += fromClientData;
       });
       return req.on('end', () => {
         console.log('POST 본문(Body):', body);
@@ -45,8 +46,8 @@ http.createServer((req, res) => {
       });
     }
   } else if (req.method === 'PUT') {
-    if (req.url.startsWith('/users/')) {
-      const key = req.url.split('/')[2];
+    if (req.url.startsWith('/users/')) { // /users/로 시작한다.
+      const key = req.url.split('/')[2]; //split 은 배열로 되고 [2]는 /user/다음이부분의 값임 이걸 key에 저장함
       let body = '';
       req.on('data', (data) => {
         body += data;
@@ -54,19 +55,20 @@ http.createServer((req, res) => {
       return req.on('end', () => {
         console.log('PUT 본문(Body):', body);
         users[key] = JSON.parse(body).name;
+        //body ==> {name:'사용자수정값'}
         return res.end(JSON.stringify(users));
       });
     }
   } else if (req.method === 'DELETE') {
     if (req.url.startsWith('/users/')) {
       const key = req.url.split('/')[2];
-      delete users[key];
+      delete users[key]; //삭제하는 명령어 delete
       return res.end(JSON.stringify(users));
     }
   }
-  res.writeHead(404, 'NOT FOUND');
+  res.writeHead(404, 'NOT FOUND'); //if를 다 빠져나옴 그럼 error
   return res.end('NOT FOUND');
 })
-  .listen(8085, () => {
+  .listen(8085, () => {// 대기 성공하면 실행하는 콜백함수
     console.log('8085번 포트에서 서버 대기중입니다');
   });
